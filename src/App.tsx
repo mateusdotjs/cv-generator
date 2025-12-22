6b0b8b06ba77969a954f8cbcb42ab88e07f72413
Mateus Soares <mateus.silvainfo@gmail.com>
2025-12-22 10:06:49 -0300

    feat: add mobile version warning

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import ResumeForm from "./components/resume-form/resume-form";
 import ResumePDF from "./components/resume-pdf/resume-pdf";
 
 function App() {
+  const isMobile = /Android|iPhone|iPod|Windows Phone/i.test(
+    navigator.userAgent
+  );
+
+  if (isMobile) {
+    return (
+      <div className="w-full">
+        <h1>🚧 Versão mobile em construção</h1>
+        <p>
+          Nosso site ainda não está disponível para celulares.
+          <br />
+          Acesse pelo computador.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-full flex flex-col lg:flex-row gap-8 lg:p-2 lg:gap-2">
       <div className="w-full flex flex-col gap-2">
